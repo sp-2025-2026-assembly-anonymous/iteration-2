469c64ed5576be9325e3c4a4de476a2337c0ddcb
PrankBG <95982786+PrankBG@users.noreply.github.com>
2026-01-21 23:52:12 +0200

    Added prototype and pdf presentation

---
+++ stu2201321085/PawSupport- Prototype/script.js
@@ -0,0 +1,12 @@
+function launchCreateSupportRequest() {
+    window.location.href = "create support request.html";
+}
+
+function launchSearchingForVolunteer() {
+    window.location.href = "searching for volunteer.html";
+
+    setTimeout(function() {
+        window.location.href = "chat with volunteer.html";
+    }, 3000);
+}
+
